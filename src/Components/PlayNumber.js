d492ade3b74271c9fb3b17bb952ae69cf8a0aadb
sly18Peso4all <jomasco18@gmail>
2021-06-21 09:25:36 +0100

    first commit

---
+++ src/Components/PlayNumber.js
@@ -0,0 +1,20 @@
+// Color Theme
+
+const PlayNumber = (props) => (
+  <button
+    className="number"
+    style={{ backgroundColor: colors[props.status] }}
+    onClick={() => props.onClick(props.number, props.status)}
+  >
+    {props.number}
+  </button>
+);
+
+const colors = {
+  available: "lightgray",
+  used: "lightgreen",
+  wrong: "lightcoral",
+  candidate: "deepskyblue",
+};
+
+export default PlayNumber;
